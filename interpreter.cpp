f38cd015e84baf17e25c09f9eaba5d4e76e13a14
WhisperKnight <61612853+Bryson-C@users.noreply.github.com>
2021-12-14 09:04:25 -0700

    Update interpreter.cpp

--- interpreter.cpp
+++ interpreter.cpp
@@ -1,25 +1,27 @@
+#define slotCount 50
+
 #include <iostream>
 #include <string.h>
 
-const char* input = " ,.[-].";
+const char* input = "++>++>++";
 
-void printcells(int slots[8]) {
-    for (int i = 0; i < 8; i++) {
+void printcells(int slots[slotCount]) {
+    for (int i = 0; i < slotCount; i++) {
         printf("| %i ", slots[i]);
-        if (i == 7) printf("|\n");
+        if (i == slotCount-1) printf("|\n");
     }
 }
 
 int main() {
-    int cells[8] = {};
+    int cells[slotCount] = {};
     int current = 0;
     char inpchr;
     
     for (int i = 0; i < strlen(input); i++) {
         if (input[i] == '+') cells[current]++;
         if (input[i] == '-') cells[current]--;
-        if (input[i] == '>') if (current == 7) current = 0; else current++;
-        if (input[i] == '<') if (current == 0) current = 7; else current--;
+        if (input[i] == '>') if (current == slotCount-1) current = 0; else current++;
+        if (input[i] == '<') if (current == 0) current = slotCount-1; else current--;
         if (input[i] == '.') printcells(cells);
         if (input[i] == ',') { printf("Input: "); std::cin >> inpchr; cells[current] = (int)inpchr; }
         if (input[i] == '[') if (cells[current] == 0) { while(input[i] != ']') i++; i++; }
